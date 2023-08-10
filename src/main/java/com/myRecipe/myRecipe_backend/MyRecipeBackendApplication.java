a871e2243e551262f0353ddac7d6c101910b8388
seo-young <sy0hiu@gmail.com>
2023-08-11 00:54:43 +0900

    [chore] 프로젝트 세팅

---
+++ src/main/java/com/myRecipe/myRecipe_backend/MyRecipeBackendApplication.java
@@ -0,0 +1,13 @@
+package com.myRecipe.myRecipe_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyRecipeBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyRecipeBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/myRecipe/myRecipe_backend/MyRecipeBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.myRecipe.myRecipe_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyRecipeBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
