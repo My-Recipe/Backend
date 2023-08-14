507f64397fd87edfba88452380bf54de06432212
seo-young <sy0hiu@gmail.com>
2023-08-15 01:00:30 +0900

    [feat] Inventory 저장하는 기능 추가

---
+++ src/main/java/com/myRecipe/myRecipe_backend/domain/inventory/application/service/InventoryCreateService.java
@@ -0,0 +1,27 @@
+package com.myRecipe.myRecipe_backend.domain.inventory.application.service;
+
+import com.myRecipe.myRecipe_backend.domain.inventory.application.dto.request.InventoryRequest;
+import com.myRecipe.myRecipe_backend.domain.inventory.application.mapper.InventoryMapper;
+import com.myRecipe.myRecipe_backend.domain.inventory.domain.entity.Inventory;
+import com.myRecipe.myRecipe_backend.domain.inventory.domain.service.InventorySaveService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.time.LocalDate;
+
+@Service
+@RequiredArgsConstructor
+public class InventoryCreateService {
+
+    private final InventorySaveService inventorySaveService;
+
+    public void createInventory(InventoryRequest.InventoryCreateRequest inventoryCreateRequest) {
+        String name = inventoryCreateRequest.getName();
+        String quantity = inventoryCreateRequest.getQuantity();
+        LocalDate expirationDate = inventoryCreateRequest.getExpirationDate();
+        LocalDate registrationDate = inventoryCreateRequest.getRegistrationDate();
+
+        Inventory inventory = InventoryMapper.mapToInventory(name, quantity, expirationDate, registrationDate);
+        inventorySaveService.saveInventory(inventory);
+    }
+}
