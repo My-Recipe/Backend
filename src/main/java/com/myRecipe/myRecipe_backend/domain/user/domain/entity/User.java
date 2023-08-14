c795733ae2b7bd97630df4b1979c7c9e813ab373
hs98012 <hs98012@daum.net>
2023-08-15 00:42:45 +0900

    [style] Annotation 줄 변경

--- src/main/java/com/myRecipe/myRecipe_backend/domain/user/domain/entity/User.java
+++ src/main/java/com/myRecipe/myRecipe_backend/domain/user/domain/entity/User.java
@@ -10,8 +10,8 @@ import lombok.NoArgsConstructor;
 @Getter
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class User {
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
+
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "user_id")
     private Long id;
 
