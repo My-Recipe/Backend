1d75cb84cfe13cb67f9f9adfd2767c5883550584
hs98012 <hs98012@daum.net>
2023-08-17 00:47:35 +0900

    [fix] 양방향 연관관계 삭제

--- src/main/java/com/friedNote/friedNote_backend/domain/recipeBook/domain/entity/RecipeBook.java
+++ src/main/java/com/friedNote/friedNote_backend/domain/recipeBook/domain/entity/RecipeBook.java
@@ -1,15 +1,11 @@
 package com.friedNote.friedNote_backend.domain.recipeBook.domain.entity;
 
-import com.friedNote.friedNote_backend.domain.recipe.domain.entity.Recipe;
 import com.friedNote.friedNote_backend.domain.user.domain.entity.User;
 import jakarta.persistence.*;
 import lombok.AccessLevel;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
-
-import java.util.ArrayList;
-import java.util.List;
 
 @Entity
 @Getter
@@ -25,9 +21,6 @@ public class RecipeBook {
     @OneToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "user_id")
     private User user;
-
-    @OneToMany(mappedBy = "recipeBook", cascade =CascadeType.ALL, orphanRemoval = true)
-    private List<Recipe> recipeList = new ArrayList<>();
 
     @Builder
     public RecipeBook(String title, String subtitle, User user) {
--- src/main/java/com/friedNote/friedNote_backend/domain/user/domain/entity/User.java
+++ src/main/java/com/friedNote/friedNote_backend/domain/user/domain/entity/User.java
@@ -1,15 +1,11 @@
 package com.friedNote.friedNote_backend.domain.user.domain.entity;
 
-import com.friedNote.friedNote_backend.domain.recipe.domain.entity.Recipe;
 import com.friedNote.friedNote_backend.domain.recipeBook.domain.entity.RecipeBook;
 import jakarta.persistence.*;
 import lombok.AccessLevel;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
-
-import java.util.ArrayList;
-import java.util.List;
 
 @Entity
 @Getter
@@ -23,9 +19,6 @@ public class User {
     private String name;
     private String email;
     private String profileUrl;
-
-    @OneToMany(mappedBy = "user", cascade = CascadeType.ALL, orphanRemoval = true)
-    private List<Recipe> recipeList = new ArrayList<>();
 
     @OneToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "recipeBook_id")
