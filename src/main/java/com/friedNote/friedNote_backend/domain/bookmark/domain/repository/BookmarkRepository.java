7f4452a9a2e27ea564b702453fe85518b57ae12a
hs98012 <hs98012@daum.net>
2023-08-18 00:26:38 +0900

    [fix] Repository 어노테이션 추가

--- src/main/java/com/friedNote/friedNote_backend/domain/bookmark/domain/repository/BookmarkRepository.java
+++ src/main/java/com/friedNote/friedNote_backend/domain/bookmark/domain/repository/BookmarkRepository.java
@@ -2,6 +2,8 @@ package com.friedNote.friedNote_backend.domain.bookmark.domain.repository;
 
 import com.friedNote.friedNote_backend.domain.bookmark.domain.entity.Bookmark;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface BookmarkRepository extends JpaRepository<Bookmark, Long> {
 }
