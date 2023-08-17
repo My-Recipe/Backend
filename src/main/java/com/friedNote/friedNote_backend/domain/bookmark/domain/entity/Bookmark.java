efee4d2b9eb97ad351fd3bf12cf80fa906a1cf98
hs98012 <hs98012@daum.net>
2023-08-18 00:17:46 +0900

    [feat] Bookmark Entity 추가

---
+++ src/main/java/com/friedNote/friedNote_backend/domain/bookmark/domain/entity/Bookmark.java
@@ -0,0 +1,31 @@
+package com.friedNote.friedNote_backend.domain.bookmark.domain.entity;
+
+import com.friedNote.friedNote_backend.domain.recipe.domain.entity.Recipe;
+import com.friedNote.friedNote_backend.domain.user.domain.entity.User;
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class Bookmark {
+
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "bookmark_id")
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    private Recipe recipe;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    private User user;
+
+    @Builder
+    public Bookmark(Recipe recipe, User user) {
+        this.recipe = recipe;
+        this.user = user;
+    }
+}
