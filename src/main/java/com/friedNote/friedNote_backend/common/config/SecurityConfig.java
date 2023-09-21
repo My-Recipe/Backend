45c76a7ae217f3ee77b74f1139362c1374afd931
seo-young <sy0hiu@gmail.com>
2023-09-22 00:00:56 +0900

    [fix] @Configuration 추가

--- src/main/java/com/friedNote/friedNote_backend/common/config/SecurityConfig.java
+++ src/main/java/com/friedNote/friedNote_backend/common/config/SecurityConfig.java
@@ -6,12 +6,14 @@ import com.friedNote.friedNote_backend.common.security.jwt.JwtAuthenticationEntr
 import com.friedNote.friedNote_backend.common.security.jwt.JwtAuthenticationFilter;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
+@Configuration
 @EnableWebSecurity
 @RequiredArgsConstructor
 public class SecurityConfig {
