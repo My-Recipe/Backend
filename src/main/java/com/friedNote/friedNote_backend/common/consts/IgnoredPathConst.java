704066db92c0eaa91e1c2fc4ba1579486d6eef46
seo-young <sy0hiu@gmail.com>
2023-09-21 23:00:54 +0900

    [feat] const 관련 추가

---
+++ src/main/java/com/friedNote/friedNote_backend/common/consts/IgnoredPathConst.java
@@ -0,0 +1,17 @@
+package com.friedNote.friedNote_backend.common.consts;
+
+import lombok.AccessLevel;
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor(access = AccessLevel.PRIVATE)
+public final class IgnoredPathConst {
+
+    public static final String[] IGNORED_PATHS = {
+            "/oauth2/**",
+            "/v3/api-docs/**",
+            "/info",
+            "/login/oauth2/**",
+            "/reissue",
+            "/favicon.ico"
+            };
+}
