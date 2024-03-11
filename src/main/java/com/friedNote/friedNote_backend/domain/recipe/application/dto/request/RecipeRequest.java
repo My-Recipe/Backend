b9422dcdfb6047feb91f1f382114af6aba5a0a88
syyling <sy0hiu@gmail.com>
2024-03-11 17:58:12 +0900

    [fix] 레시피 생성 dto 수정

--- src/main/java/com/friedNote/friedNote_backend/domain/recipe/application/dto/request/RecipeRequest.java
+++ src/main/java/com/friedNote/friedNote_backend/domain/recipe/application/dto/request/RecipeRequest.java
@@ -11,33 +11,25 @@ import lombok.Setter;
 import java.util.List;
 
 public class RecipeRequest {
+
     @Getter
     @Setter
     @NoArgsConstructor
     public static class RecipeCreateRequest {
 
-        /**
-         * 리스트 변수명 리팩토링 필요
-         */
         @Schema(description = "레시피 이름", defaultValue = "name")
         private String recipeName;
         @Schema(description = "레시피 공개여부", defaultValue = "publicityStatus")
         private boolean publicityStatus;
-        @Schema(description = "레시피북 식별자", defaultValue = "recipeBookId")
-        private Long recipeBookId;
-        @Schema(description = "사용자 식별자", defaultValue = "userId")
-        private Long userId;
         @Schema(description = "조리과정 리스트", defaultValue = "cookingProcessCreateRequestList")
         private List<CookingProcessRequest.CookingProcessCreateRequest> cookingProcessCreateRequestList;
         @Schema(description = "재료그룹 리스트", defaultValue = "ingredientGroupCreateRequestList")
         private List<IngredientGroupRequest.IngredientGroupCreateRequest> ingredientGroupCreateRequestList;
 
         @Builder
-        public RecipeCreateRequest(String recipeName, boolean publicityStatus, Long recipeBookId, Long userId, List<CookingProcessRequest.CookingProcessCreateRequest> cookingProcessCreateRequestList, List<IngredientGroupRequest.IngredientGroupCreateRequest> ingredientGroupCreateRequestList) {
+        public RecipeCreateRequest(String recipeName, boolean publicityStatus, List<CookingProcessRequest.CookingProcessCreateRequest> cookingProcessCreateRequestList, List<IngredientGroupRequest.IngredientGroupCreateRequest> ingredientGroupCreateRequestList) {
             this.recipeName = recipeName;
             this.publicityStatus = publicityStatus;
-            this.recipeBookId = recipeBookId;
-            this.userId = userId;
             this.cookingProcessCreateRequestList = cookingProcessCreateRequestList;
             this.ingredientGroupCreateRequestList = ingredientGroupCreateRequestList;
         }
